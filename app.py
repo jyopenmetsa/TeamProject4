4cb22ac30083a42cd15a68614a41ee4774d5b040
Jyothi Pushpanjali Penmetsa <jpenmetsa@seattleu.edu>
2023-03-10 18:52:52 -0800

    save commits

--- app.py
+++ app.py
@@ -6,25 +6,6 @@ import json
 app = Flask(__name__, template_folder='templates')
 
 
-# define methods for routes (what to do and display)
-# @app.route("/")
-# def main():
-#     return render_template('index.html', form_action_url=url_for('search'))
-#
-# @app.route('/search', methods=['POST'])
-# def search():
-#     print("in search method")
-#     f = open('./data/TP_data.json')
-#     data = json.load(f)
-#     print(data['countries'][0])
-#     print(len(data['countries']))
-#     output_dict = [x for x in data['countries'] if x['ZipCode'] == 'T2V2W2']
-#     print(output_dict)
-#     print("length",len(output_dict))
-#     name = request.form.get("name")
-#     print(name)
-#     return render_template('index.html')
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     print("in index def")
@@ -39,84 +20,8 @@ def index():
         city = request.form.get('city')
         state = request.form.get('state')
         code = request.form.get('code')
-        f = open('./data/TP_data1.json')
-        data = json.load(f)
-        countriesData = data['countries']
 
-        print("canada : ", canada)
-        print("usa : ", usa)
-        print("state : ", state)
-        print("address1 : ", address1)
-        print("address2 : ", address2)
-
-        selectedCountriesData = []
-
-        if usa is not None:
-            selectedCountriesData += [x for x in countriesData if x['Country'] == 'United States of America']
-        if canada is not None:
-            selectedCountriesData += [x for x in countriesData if x['Country'] == 'Canada']
-
-        print(len(selectedCountriesData))
-
-        filteredData = []
-        filteredData[:] = []
-        print("length of filter data before : ", len(filteredData))
-
-        if fname:
-            if not filteredData:
-                filteredData += [x for x in selectedCountriesData if x['First_Name'] == fname]
-                print("length after fname if cond: ", len(filteredData))
-            else:
-                filteredData = [x for x in filteredData if x['First_Name'] == fname]
-                print("length after fname else cond: ", len(filteredData))
-
-        if lname:
-            if not filteredData:
-                filteredData += [x for x in selectedCountriesData if x['Last_Name'] == lname]
-                print("length after lname if cond: ", len(filteredData))
-            else:
-                filteredData = [x for x in filteredData if x['Last_Name'] == lname]
-                print("length after lname else cond: ", len(filteredData))
-
-        if address1:
-            if not filteredData:
-                filteredData += [x for x in selectedCountriesData if x['Address1'] == address1]
-                print("length after addr1 if cond: ", len(filteredData))
-            else:
-                filteredData = [x for x in filteredData if x['Address1'] == address1]
-                print("length after addr1 else cond: ", len(filteredData))
-
-        if address2:
-            if not filteredData:
-                filteredData += [x for x in selectedCountriesData if x['Address2'] == address2]
-                print("length after addr2 if cond: ", len(filteredData))
-            else:
-                filteredData = [x for x in filteredData if x['Address2'] == address2]
-                print("length after addr2 else cond: ", len(filteredData))
-
-        if city:
-            if not filteredData:
-                filteredData += [x for x in selectedCountriesData if x['City'] == city]
-                print("length after city if cond: ", len(filteredData))
-            else:
-                filteredData = [x for x in filteredData if x['City'] == city]
-                print("length after city else cond: ", len(filteredData))
-
-        if state:
-            if not filteredData:
-                filteredData += [x for x in selectedCountriesData if x['State'] == state]
-                print("length after state if cond: ", len(filteredData))
-            else:
-                filteredData = [x for x in filteredData if x['State'] == state]
-                print("length after state else cond: ", len(filteredData))
-
-        if code:
-            if not filteredData:
-                filteredData += [x for x in selectedCountriesData if x['ZipCode'] == code]
-                print("length after code if cond: ", len(filteredData))
-            else:
-                filteredData = [x for x in filteredData if x['ZipCode'] == code]
-                print("length after code else cond: ", len(filteredData))
+        filteredData = filterData(usa, canada, fname, lname, address1, address2, city, state, code)
 
         print("filtered data  : ", len(filteredData))
         form_data = request.form
@@ -128,6 +33,130 @@ def index():
                            fname=fname, lname=lname,
                            address1=address1, address2=address2,
                            state=state, city=city, code=code)
+
+
+def filterData(usa, canada, fname, lname, address1, address2, city, state, code):
+    print("in filterData method")
+    print(fname,lname,address1,address2,city,state,code)
+    f = open('./data/TP_data1.json')
+    data = json.load(f)
+    countriesData = data['countries']
+
+    print("canada : ", canada)
+    print("usa : ", usa)
+    print("state : ", state)
+    print("address1 : ", address1)
+    print("address2 : ", address2)
+
+    selectedCountriesData = []
+
+    if usa is not None:
+        selectedCountriesData += [x for x in countriesData if x['Country'] == 'United States of America']
+    if canada is not None:
+        selectedCountriesData += [x for x in countriesData if x['Country'] == 'Canada']
+
+    print(len(selectedCountriesData))
+
+    filteredData = []
+    filteredData[:] = []
+    print("length of filter data before : ", len(filteredData))
+
+    print(fname, lname, address1, address2, city, state, code)
+
+    if fname:
+        if not filteredData:
+            filteredData += [x for x in selectedCountriesData if fname in x['First_Name']]
+            print("length after fname if cond: ", len(filteredData))
+        else:
+            filteredData = [x for x in filteredData if fname in x['First_Name']]
+            print("length after fname else cond: ", len(filteredData))
+
+    if lname:
+        if not filteredData:
+            filteredData += [x for x in selectedCountriesData if lname in x['Last_Name']]
+            print("length after lname if cond: ", len(filteredData))
+        else:
+            filteredData = [x for x in filteredData if lname in x['Last_Name']]
+            print("length after lname else cond: ", len(filteredData))
+
+    if address1:
+        if not filteredData:
+            filteredData += [x for x in selectedCountriesData if address1 in x['Address1']]
+            print("length after addr1 if cond: ", len(filteredData))
+        else:
+            filteredData = [x for x in filteredData if address1 in x['Address1']]
+            print("length after addr1 else cond: ", len(filteredData))
+
+    if address2:
+        if not filteredData:
+            filteredData += [x for x in selectedCountriesData if address2 in x['Address2']]
+            print("length after addr2 if cond: ", len(filteredData))
+        else:
+            filteredData = [x for x in filteredData if address2 in x['Address2']]
+            print("length after addr2 else cond: ", len(filteredData))
+
+    if city:
+        if not filteredData:
+            filteredData += [x for x in selectedCountriesData if city in x['City']]
+            print("length after city if cond: ", len(filteredData))
+        else:
+            filteredData = [x for x in filteredData if city in x['City']]
+            print("length after city else cond: ", len(filteredData))
+
+    if state:
+        if not filteredData:
+            filteredData += [x for x in selectedCountriesData if state in x['State']]
+            print("length after state if cond: ", len(filteredData))
+        else:
+            filteredData = [x for x in filteredData if state in x['State']]
+            print("length after state else cond: ", len(filteredData))
+
+    if code:
+        if not filteredData:
+            filteredData += [x for x in selectedCountriesData if code in x['ZipCode']]
+            print("length after code if cond: ", len(filteredData))
+        else:
+            filteredData = [x for x in filteredData if code in x['ZipCode']]
+            print("length after code else cond: ", len(filteredData))
+
+    return filteredData
+
+@app.route('/filter', methods=['GET'])
+def filter():
+    selectedCountriesData = []
+
+    # usa = request.args.get('usa')
+    # canada = request.args.get('canada')
+    usa, canada, mexico, india, japan = None
+    country = request.args.get('country')
+    if country == "usa":
+        usa = "on"
+
+    if country == "canada":
+        canada = "on"
+
+    if country == "mexico":
+        mexico = "on"
+
+    if country == "india":
+        india == "on"
+
+    if country == "japan":
+        japan = "on"
+
+    fname = request.args.get('fname')
+    lname = request.args.get('lname')
+    address1 = request.args.get('address1')
+    address2 = request.args.get('address2')
+    city = request.args.get('city')
+    state = request.args.get('state')
+    code = request.args.get('code')
+
+    print(fname, lname, address1, address2, city, state, code)
+
+    filteredData = filterData(usa, canada, fname, lname, address1, address2, city, state, code)
+
+    return filteredData
 
 
 if __name__ == "__main__":
