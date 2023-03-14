47372e52b39d7f82b815c9833c12a6de75573190
Jyothi Pushpanjali Penmetsa <jpenmetsa@seattleu.edu>
2023-03-13 22:22:30 -0700

    formatter

--- app.py
+++ app.py
@@ -11,6 +11,194 @@ message_404 = {
         "status": 404,
         "message": "Invalid address"
     }
+@app.route("/japan", methods=['GET', 'POST'])
+def japan():
+    print("in japan")
+    options = ["Option 1", "Option 2", "Option 3","test"]
+    japan_cities = []
+    japan_cities.append("")
+    f = open('./data/TP_data1.json')
+    data = json.load(f)
+    countriesData = data['countries']
+    japanData = [x for x in countriesData if x['Country'] == 'Japan']
+    for entry in japanData:
+        city = entry["City"]
+        if city not in japan_cities:
+            japan_cities.append(city)
+    japan_cities.sort()
+    print(japan_cities)
+
+    if request.method == 'POST':
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        # city = request.form.get('city')
+        code = request.form.get('code')
+        city = request.form.get('city')
+        print("test",city)
+        filteredData = filterData(usa=None, canada=None, mexico=None, india=None, japan="on",
+                                  fname=fname, lname=lname, address1=address1, address2=address2,
+                                  city=city, state=None, code=code)
+
+        print("filtered data  : ", len(filteredData))
+        form_data = request.form
+    else:
+        usa = fname = lname = address1 = address2 = city = code = ''
+        filteredData = []
+        form_data = {}
+    return render_template('japan.html', data=filteredData, form=form_data,
+                           fname=fname, lname=lname,
+                           address1=address1, address2=address2, city=city, code=code, options=japan_cities)
+
+@app.route("/usa", methods=['GET', 'POST'])
+def usa():
+    print("in usa")
+    usa_cities = []
+    usa_cities.append("")
+    f = open('./data/TP_data1.json')
+    data = json.load(f)
+    countriesData = data['countries']
+    usaData = [x for x in countriesData if x['Country'] == 'United States of America']
+    for entry in usaData:
+        city = entry["City"]
+        if city not in usa_cities:
+            usa_cities.append(city)
+    usa_cities.sort()
+
+    if request.method == 'POST':
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        code = request.form.get('code')
+        city = request.form.get('city')
+        print("test",city)
+        filteredData = filterData(usa="on", canada=None, mexico=None, india=None, japan=None,
+                                  fname=fname, lname=lname, address1=address1, address2=address2,
+                                  city=city, state=None, code=code)
+
+        print("filtered data  : ", len(filteredData))
+        form_data = request.form
+    else:
+        usa = fname = lname = address1 = address2 = city = code = ''
+        filteredData = []
+        form_data = {}
+    return render_template('usa.html', data=filteredData, form=form_data,
+                           fname=fname, lname=lname,
+                           address1=address1, address2=address2, city=city, code=code, options=usa_cities)
+
+@app.route("/mexico", methods=['GET', 'POST'])
+def mexico():
+    print("in mexico")
+    mexico_cities = []
+    mexico_cities.append("")
+    f = open('./data/TP_data1.json')
+    data = json.load(f)
+    countriesData = data['countries']
+    mexicoData = [x for x in countriesData if x['Country'] == 'Mexico']
+    for entry in mexicoData:
+        city = entry["City"]
+        if city not in mexico_cities:
+            mexico_cities.append(city)
+    mexico_cities.sort()
+
+    if request.method == 'POST':
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        code = request.form.get('code')
+        city = request.form.get('city')
+        print("test",city)
+        filteredData = filterData(usa=None, canada=None, mexico="on", india=None, japan=None,
+                                  fname=fname, lname=lname, address1=address1, address2=address2,
+                                  city=city, state=None, code=code)
+
+        print("filtered data  : ", len(filteredData))
+        form_data = request.form
+    else:
+        usa = fname = lname = address1 = address2 = city = code = ''
+        filteredData = []
+        form_data = {}
+    return render_template('mexico.html', data=filteredData, form=form_data,
+                           fname=fname, lname=lname,
+                           address1=address1, address2=address2, city=city, code=code, options=mexico_cities)
+
+@app.route("/canada", methods=['GET', 'POST'])
+def canada():
+    print("in canada")
+    canada_cities = []
+    canada_cities.append("")
+    f = open('./data/TP_data1.json')
+    data = json.load(f)
+    countriesData = data['countries']
+    canadaData = [x for x in countriesData if x['Country'] == 'Canada']
+    for entry in canadaData:
+        city = entry["City"]
+        if city not in canada_cities:
+            canada_cities.append(city)
+    canada_cities.sort()
+
+    if request.method == 'POST':
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        code = request.form.get('code')
+        city = request.form.get('city')
+        print("test",city)
+        filteredData = filterData(usa=None, canada="on", mexico=None, india=None, japan=None,
+                                  fname=fname, lname=lname, address1=address1, address2=address2,
+                                  city=city, state=None, code=code)
+
+        print("filtered data  : ", len(filteredData))
+        form_data = request.form
+    else:
+        usa = fname = lname = address1 = address2 = city = code = ''
+        filteredData = []
+        form_data = {}
+    return render_template('canada.html', data=filteredData, form=form_data,
+                           fname=fname, lname=lname,
+                           address1=address1, address2=address2, city=city, code=code, options=canada_cities)
+
+@app.route("/india", methods=['GET', 'POST'])
+def india():
+    print("in india")
+    india_cities = []
+    india_cities.append("")
+    f = open('./data/TP_data1.json')
+    data = json.load(f)
+    countriesData = data['countries']
+    indiaData = [x for x in countriesData if x['Country'] == 'India']
+    for entry in indiaData:
+        city = entry["City"]
+        if city not in india_cities:
+            india_cities.append(city)
+    india_cities.sort()
+
+    if request.method == 'POST':
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        code = request.form.get('code')
+        city = request.form.get('city')
+        print("test",city)
+        filteredData = filterData(usa=None, canada=None, mexico=None, india="on", japan=None,
+                                  fname=fname, lname=lname, address1=address1, address2=address2,
+                                  city=city, state=None, code=code)
+
+        print("filtered data  : ", len(filteredData))
+        form_data = request.form
+    else:
+        usa = fname = lname = address1 = address2 = city = code = ''
+        filteredData = []
+        form_data = {}
+    return render_template('india.html', data=filteredData, form=form_data,
+                           fname=fname, lname=lname,
+                           address1=address1, address2=address2, city=city, code=code, options=india_cities)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     print("in index def")
@@ -58,6 +246,8 @@ def filterData(usa, canada, mexico, india, japan, fname, lname, address1, addres
     print("state : ", state)
     print("address1 : ", address1)
     print("address2 : ", address2)
+    print("fname : ", fname)
+    print("lname : ", lname)
 
     selectedCountriesData = []
 
@@ -142,8 +332,6 @@ def filterData(usa, canada, mexico, india, japan, fname, lname, address1, addres
 def filter():
     selectedCountriesData = []
 
-    # usa = request.args.get('usa')
-    # canada = request.args.get('canada')
     usa = canada = mexico = india = japan = None
     country = request.args.get('country')
     countryList = request.args.get('country').split(',')
